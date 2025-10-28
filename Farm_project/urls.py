27a38179bdbbb345c6f5a28cee11ae541f339e06
Miles Munyinyi <milesmunyinyi447@gmail.com>
2025-10-28 10:51:15 +0300

    Fixed homepage route

--- Farm_project/urls.py
+++ Farm_project/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the Farm Project!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
     path('', include('farm.urls')),
 ]
 
