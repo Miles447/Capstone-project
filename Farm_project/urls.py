09f8c230242bb09db2f60828eddcca2aa9fe8dfe
Miles Munyinyi <milesmunyinyi447@gmail.com>
2025-10-28 12:00:38 +0300

    Fix homepage view to render template

--- Farm_project/urls.py
+++ Farm_project/urls.py
@@ -16,14 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Welcome to the Farm Project!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('', include('farm.urls')),
+    path('', include('farm.urls')),  #  use the farm app's home view
 ]
+
 
